24e4f0d13e6437b797c9bacc5e14c0f8f2b84a73
Saikat Kumar Dey <deysaikatkumar@gmail.com>
2023-01-07 20:19:27 +0530

    Bump to 0.1.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="aidoc",
-    version="0.1.4",
+    version="0.1.5",
     author="Saikat Kumar Dey",
     author_email="deysaikatkumar@gmail.com",
     description="A simple CLI tool to generate documentation for your Python projects automatically.",
