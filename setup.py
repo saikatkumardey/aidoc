acfd3ef238c9e694a22d1ada5e02bf12c4945b44
Saikat Kumar Dey <deysaikatkumar@gmail.com>
2023-01-07 19:22:36 +0530

    update patch

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="aidoc",
-    version="0.1.0",
+    version="0.1.1",
     author="Saikat Kumar Dey",
     author_email="deysaikatkumar@gmail.com",
     description="A simple CLI tool to generate documentation for your Python projects automatically.",
